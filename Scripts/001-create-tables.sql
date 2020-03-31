570a9f2bee88997b93d08a75a3bfc3f6cf080f3a
Mike Matthews <mike@factor-50.co.uk>
2020-03-31 16:44:18 +0100

    Updated following discussion with Ryan

--- Scripts/001-create-tables.sql
+++ Scripts/001-create-tables.sql
@@ -32,8 +32,7 @@ CREATE INDEX ix_FirebaseUID ON [User].[User] (FirebaseUID)
 
 CREATE TABLE [UserPersonal].[PersonalDetails]
 (
-    ID	            int identity(1,1) not null,
-    UserID          int not null, -- Historically I'd have made this the primary key and removed the identity column; not sure how that would play with entity framework.  I'm not precious either way.
+    UserID          int not null,
     FirstName	    varchar(50) null,
     LastName	    varchar(50) null,
     DisplayName	    varchar(50) null,
@@ -46,17 +45,14 @@ CREATE TABLE [UserPersonal].[PersonalDetails]
     EmailAddress	varchar(50) null,
     MobilePhone	    varchar(15) null,
     OtherPhone	    varchar(15) null,
-    CONSTRAINT PK_PersonalDetails PRIMARY KEY (ID),
+    CONSTRAINT PK_PersonalDetails PRIMARY KEY (UserID),
     CONSTRAINT FK_PersonalDetails_User FOREIGN KEY (UserID) REFERENCES [User].[User] (ID)
 )
-
-CREATE INDEX ix_UserID ON [UserPersonal].[PersonalDetails] (UserID)
 
 
 
 CREATE TABLE [User].[SupportPostcode]
 (
-    -- Do we need an identity column here too for the primary key?  I'd prefer not, but speed is of the essence...
     UserID      int not null,
     PostalCode  varchar(10) not null,
     CONSTRAINT PK_SupportPostcode PRIMARY KEY (UserID, PostalCode),
@@ -64,14 +60,11 @@ CREATE TABLE [User].[SupportPostcode]
 )
 
 CREATE INDEX ix_PostalCode ON [User].[SupportPostcode] (PostalCode) INCLUDE (UserID)
--- Add the following index if the primary key changes
---CREATE INDEX ix_UserID ON [User].[SupportPostcode] (UserID) INCLUDE (PostalCode)
 
 
 
 CREATE TABLE [User].[ChampionPostcode]
 (
-    -- Do we need an identity column here too for the primary key?  I'd prefer not, but speed is of the essence...
     UserID      int not null,
     PostalCode  varchar(10) not null,
     CONSTRAINT PK_ChampionPostcode PRIMARY KEY (UserID, PostalCode),
@@ -79,19 +72,14 @@ CREATE TABLE [User].[ChampionPostcode]
 )
 
 CREATE INDEX ix_PostalCode ON [User].[ChampionPostcode] (PostalCode) INCLUDE (UserID)
--- Add the following index if the primary key changes
---CREATE INDEX ix_UserID ON [User].[ChampionPostcode] (UserID) INCLUDE (PostalCode)
 
 
 
 CREATE TABLE [User].[SupportActivity]
 (
-    -- Do we need an identity column here too for the primary key?  I'd prefer not, but speed is of the essence...
     UserID          int not null, 
     ActivityID      tinyint not null,
     CONSTRAINT PK_SupportActivity PRIMARY KEY (UserID, ActivityID),
     CONSTRAINT FK_SupportActivity_User FOREIGN KEY (UserID) REFERENCES [User].[User] (UserID)
 )
 
--- Add the following index if the primary key changes
---CREATE INDEX ix_UserID ON [User].[SupportActivity] (UserID) INCLUDE (ActivityID)
