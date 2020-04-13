ace8d63bfe92d94b000a73c179df4b0234bcd67a
Mike Matthews <mike@factor-50.co.uk>
2020-04-13 21:02:38 +0100

    GetDistinctVolunteerUserCount

---
+++ UserService/UserService.Core/Domains/Entities/GetDistinctVolunteerUserCountRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Core.Domains.Entities
+{
+    public class GetDistinctVolunteerUserCountRequest : IRequest<GetDistinctVolunteerUserCountResponse>
+    {
+    }
+}
---
+++ UserService/UserService.Handlers/GetDistinctVolunteerUserCountHandler.cs
@@ -0,0 +1,33 @@
+﻿using UserService.Core.Domains.Entities;
+using UserService.Core.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UserService.Handlers
+{
+    public class GetDistinctVolunteerUserCountHandler : IRequestHandler<GetDistinctVolunteerUserCountRequest, GetDistinctVolunteerUserCountResponse>
+    {
+        private readonly IRepository _repository;
+
+        public GetDistinctVolunteerUserCountHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<GetDistinctVolunteerUserCountResponse> Handle(GetDistinctVolunteerUserCountRequest request, CancellationToken cancellationToken)
+        {
+            var count = _repository.GetDistinctVolunteerUserCount();
+
+            var response = new GetDistinctVolunteerUserCountResponse()
+            {
+                Count = count
+            };
+            
+            return Task.FromResult(response);
+        }
+    }
+}
