b06a7907dd0014c5e766430bc503eca51822d5e5
William Thomas <14827486+william-thomas@users.noreply.github.com>
2020-05-10 18:12:08 +0100

    Add NumberOfHelpers and NumberOfStreetChampions to GetVolunteerCoordinate endpoint when MinDistanceBetweenInMetres is 0

--- UserService/UserService.Core/Domains/Entities/VolunteerCoordinate.cs
+++ UserService/UserService.Core/Domains/Entities/VolunteerCoordinate.cs
@@ -14,5 +14,12 @@ namespace UserService.Core.Domains.Entities
         [DataMember(Name = "lng")]
         public double Longitude { get; set; }
 
+        // fields are temporarily nullable until the grid aggregation functionality is implemented
+        [DataMember(Name = "sc")]
+        public double? NumberOfStreetChampions { get; set; }
+
+        [DataMember(Name = "h")]
+        public double? NumberOfHelpers { get; set; }
+
     }
 }
--- UserService/UserService.Handlers/GetVolunteerCoordinatesHandler.cs
+++ UserService/UserService.Handlers/GetVolunteerCoordinatesHandler.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using HelpMyStreet.Utils.CoordinatedResetCache;
+﻿using HelpMyStreet.Utils.CoordinatedResetCache;
 using MediatR;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UserService.Core;
@@ -29,6 +29,8 @@ namespace UserService.Handlers
         {
             IEnumerable<CachedVolunteerDto> cachedVolunteerDtos;
 
+            IReadOnlyList<VolunteerCoordinate> cachedVolunteerDtosWithinBoundary;
+
             // calculating coordinates that have a minimum distance between them is expensive so cache the result
             if (request.MinDistanceBetweenInMetres > 0)
             {
@@ -37,19 +39,36 @@ namespace UserService.Handlers
                 string key = $"{nameof(CachedVolunteerDto)}_MinDistance_{request.MinDistanceBetweenInMetres}_{request.VolunteerType}_{request.IsVerifiedType}";
 
                 cachedVolunteerDtos = await _coordinatedResetCache.GetCachedDataAsync(async (token) => await _volunteersFilteredByMinDistanceGetter.GetVolunteersFilteredByMinDistanceAsync(request, token), key, cancellationToken, CoordinatedResetCacheTime.OnHour);
+
+                cachedVolunteerDtosWithinBoundary = cachedVolunteerDtos.WhereWithinBoundary(request.SWLatitude, request.SWLongitude, request.NELatitude, request.NELongitude)
+                    .Select(x => new VolunteerCoordinate()
+                    {
+                        Latitude = x.Latitude,
+                        Longitude = x.Longitude,
+                        Postcode = x.Postcode,
+                        // fields are temporarily null until the grid aggregation functionality is implemented
+                        NumberOfHelpers = null,
+                        NumberOfStreetChampions = null
+                    }).ToList();
             }
             else
             {
                 cachedVolunteerDtos = await _volunteerCache.GetCachedVolunteersAsync(request.VolunteerTypeEnum, request.IsVerifiedTypeEnum, cancellationToken);
+
+                cachedVolunteerDtosWithinBoundary = cachedVolunteerDtos.WhereWithinBoundary(request.SWLatitude, request.SWLongitude, request.NELatitude, request.NELongitude)
+                   .GroupBy(x => new { x.Postcode, x.Latitude, x.Longitude })
+                   .Select(x => new VolunteerCoordinate()
+                   {
+                       Latitude = x.Key.Latitude,
+                       Longitude = x.Key.Longitude,
+                       Postcode = x.Key.Postcode,
+                       NumberOfHelpers = x.Count(y => y.VolunteerType == VolunteerType.Helper),
+                       NumberOfStreetChampions = x.Count(y => y.VolunteerType == VolunteerType.StreetChampion),
+                   }).ToList();
+
             }
 
-            IReadOnlyList<VolunteerCoordinate> cachedVolunteerDtosWithinBoundary = cachedVolunteerDtos.WhereWithinBoundary(request.SWLatitude, request.SWLongitude, request.NELatitude, request.NELongitude)
-                .Select(x => new VolunteerCoordinate()
-                {
-                    Latitude = x.Latitude,
-                    Longitude = x.Longitude,
-                    Postcode = x.Postcode
-                }).ToList();
+
 
             GetVolunteerCoordinatesResponse getVolunteerCoordinatesResponse = new GetVolunteerCoordinatesResponse()
             {
