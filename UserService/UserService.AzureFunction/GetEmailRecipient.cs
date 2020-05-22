5c7b8b19fcdd5e7852c13aa46f9b327a114ff8e7
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-05-22 17:26:13 +0100

    Add identify email recipients

---
+++ UserService/UserService.AzureFunction/GetEmailRecipient.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MediatR;
+using System;
+using UserService.Core.Domains.Entities;
+using System.Net;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using Microsoft.AspNetCore.Http;
+using NewRelic.Api.Agent;
+using HelpMyStreet.Contracts.UserService.Response;
+using HelpMyStreet.Contracts.UserService.Request;
+
+namespace UserService.AzureFunction
+{
+    public class GetEmailRecipient
+    {
+        private readonly IMediator _mediator;
+
+        public GetEmailRecipient(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Transaction(Web = true)]
+        [FunctionName("GetEmailRecipient")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(GetEmailRecipientResponse))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+            [RequestBodyType(typeof(GetEmailRecipientRequest), "product request")] GetEmailRecipientRequest req,
+            ILogger log)
+        {
+            try
+            {
+                NewRelic.Api.Agent.NewRelic.SetTransactionName("UserService", "GetEmailRecipientResponse");
+                log.LogInformation("C# HTTP trigger function processed a request.");
+                GetEmailRecipientResponse response = await _mediator.Send(req);
+                return new OkObjectResult(response);
+            }
+            catch (Exception exc)
+            {
+                LogError.Log(log, exc, req);
+
+                return new ObjectResult(exc)
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+    }
+}
---
+++ UserService/UserService.Handlers/GetEmailRecipientHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using HelpMyStreet.Contracts.UserService.Request;
+using HelpMyStreet.Contracts.UserService.Response;
+using System.Collections.Generic;
+
+namespace UserService.Handlers
+{
+    public class GetEmailRecipientHandler : IRequestHandler<GetEmailRecipientRequest, GetEmailRecipientResponse>
+    {
+        public async Task<GetEmailRecipientResponse> Handle(GetEmailRecipientRequest request, CancellationToken cancellationToken)
+        {
+            List<EmailRecipient> emailRecipients = new List<EmailRecipient>();
+            emailRecipients.Add(new EmailRecipient()
+            {
+                UserID = 2,
+                IsVerified= true,
+                IsStreetChampionForGivenPostCode = false
+            });
+            emailRecipients.Add(new EmailRecipient()
+            {
+                UserID = 32,
+                IsVerified = false,
+                IsStreetChampionForGivenPostCode = false
+            });
+            emailRecipients.Add(new EmailRecipient()
+            {
+                UserID = 85,
+                IsVerified = true,
+                IsStreetChampionForGivenPostCode = false
+            });
+
+            GetEmailRecipientResponse response = new GetEmailRecipientResponse()
+            {
+                EmailRecipients = emailRecipients
+            };
+
+            return response;
+        }
+    }
+}
